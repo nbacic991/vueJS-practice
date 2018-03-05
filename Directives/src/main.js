4100951805b9294dc4c6cb17faa62d939286e195
Nemanja Bacic <nbacic@ymail.com>
2018-03-05 15:15:36 +0100

    Added blinking directive with timeout.

--- Directives/src/main.js
+++ Directives/src/main.js
@@ -6,21 +6,21 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-Vue.directive('highlighted', {
+Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    // el.style.backgroundColor = 'green';
-    // el.style.backgroundColor = binding.value;
-    var delay = 0;
-    if(binding.modifiers['delayed']) {
-      delay = 3000;
-    }
-    setTimeout(() => {
-      if(binding.arg == 'background') {
-        el.style.backgroundColor = binding.value;
-      } else {
-        el.style.color = binding.value;
+      // el.style.backgroundColor = 'green';
+      // el.style.backgroundColor = binding.value;
+      var delay = 0;
+      if (binding.modifiers['delayed']) {
+          delay = 3000;
       }
-    }, delay);
+      setTimeout(() => {
+          if (binding.arg == 'background') {
+              el.style.backgroundColor = binding.value;
+          } else {
+              el.style.color = binding.value;
+          }
+      }, delay);
   }
 });
 
